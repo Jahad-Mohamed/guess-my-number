9b08f1af62af1707a8541d33e76a4b7094a0de8b
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-04-28 15:11:46 +0100

    styling and refactoring done

--- script.js
+++ script.js
@@ -12,11 +12,19 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ Input a number!";
+
+    // When player guesses the number
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent =
       "🎊 Congratulations you have guessed my number! 🎊";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    // When  guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = " ⬇️ Too High!";
@@ -26,6 +34,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent =
         " ❌ YOU HAVE LOST THE GAME! ";
     }
+
+    // When  guess is too high
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = " ⬇️ Too Low!";
     score--;
